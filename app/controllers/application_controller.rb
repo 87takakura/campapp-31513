47224eed1e665f6847df9d410ad8b17e394a66a1
87takakura <74569038+87takakura@users.noreply.github.com>
2021-04-20 23:18:08 +0900

    devise 編集機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,10 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-
-
-
-
-
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :password, :prefecture_id, :profile, :image])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :password, :prefecture_id, :profile, :image])
   end
 
 end
